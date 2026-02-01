c7793ccdd335eaf348c93a45964163881dc08be6
Dengjia Zhang <adoptedirelia@gmail.com>
2026-02-01 15:52:09 -0500

    RL frame

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Multiling-reasoning
+
+
+## Evaluation
+
+```bash
+python -m src.eval.main
+```
+
+## SFT training
+
+```bash
+torchrun --nproc_per_node=1 train.py --config ./configs/train_config_example.json
+```
+
+## RL training
+
+- to be done
---
+++ src/data_process/Multiling_dataset.py
@@ -0,0 +1,17 @@
+import json
+
+
+class Global_PIQA:
+    def __init__(self, data_path: str):
+        self.data = self.load_data(data_path)
+
+    def load_data(self, data_path: str):
+        with open(data_path, 'r') as f:
+            data = json.load(f)
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
