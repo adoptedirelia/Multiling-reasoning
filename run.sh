0f60274adc24ffeac2f46dbb59ad13124283ac2f
Dengjia Zhang <adoptedirelia@gmail.com>
2026-02-26 13:12:19 -0500

    new data

--- run.sh
+++ run.sh
@@ -5,6 +5,22 @@
 #     --output_file end_to_end_piqa.json \
 #     --dataset ./dataset/PIQA.json
 
+# python -m src.eval.main \
+#     --config configs/eval_config_example.json \
+#     --baseline_type cascade \
+#     --output_file cascade_piqa.json \
+#     --translation_file ./mt1_translations_piqa.json \
+#     --intermediate_file intermediate_results_piqa.json \
+#     --dataset ./dataset/PIQA.json 
+
+python -m src.eval.main \
+    --config configs/eval_config_example.json \
+    --baseline_type prompting \
+    --output_file prompting_piqa.json \
+    --translation_file ./mt1_translations_piqa.json \
+    --intermediate_file intermediate_results_piqa.json \
+    --dataset ./dataset/PIQA.json 
+
 python -m src.eval.main \
     --config configs/eval_config_example.json \
     --baseline_type cascade \
@@ -12,23 +28,6 @@ python -m src.eval.main \
     --translation_file ./mt1_translations_piqa.json \
     --intermediate_file intermediate_results_piqa.json \
     --dataset ./dataset/PIQA.json 
-
-# python -m src.eval.main \
-#     --config configs/eval_config_example.json \
-#     --baseline_type prompting \
-#     --output_file prompting_piqa.json \
-#     --translation_file ./mt1_translations_piqa.json \
-#     --intermediate_file intermediate_results_piqa.json \
-#     --dataset ./dataset/PIQA.json 
-
-# python -m src.eval.main \
-#     --config configs/eval_config_example.json \
-#     --baseline_type cascade \
-#     --output_file cascade_piqa_lora.json \
-#     --translation_file ./mt1_translations_piqa.json \
-#     --intermediate_file intermediate_results_piqa.json \
-#     --dataset ./dataset/PIQA.json \
-#     --lora_path /export/fs05/dzhang98/model/MT2_lora/checkpoint-4500
 
 # python -m src.eval.main \
 #     --config configs/eval_config_example.json \
@@ -49,31 +48,31 @@ python -m src.eval.main \
 #     --dataset ./dataset/MKQA_eval.json
 
 
-python -m src.eval.main \
-    --config configs/eval_config_example.json \
-    --baseline_type cascade \
-    --output_file cascade_mkqa.json \
-    --translation_file ./mt1_translations_mkqa.json \
-    --intermediate_file intermediate_results_mkqa.json \
-    --dataset ./dataset/MKQA_eval.json 
+# python -m src.eval.main \
+#     --config configs/eval_config_example.json \
+#     --baseline_type cascade \
+#     --output_file cascade_mkqa.json \
+#     --translation_file ./mt1_translations_mkqa.json \
+#     --intermediate_file intermediate_results_mkqa.json \
+#     --dataset ./dataset/MKQA_eval.json 
 
-python -m src.eval.main \
-    --config configs/eval_config_example.json \
-    --baseline_type prompting \
-    --output_file prompting_mkqa.json \
-    --translation_file ./mt1_translations_mkqa.json \
-    --intermediate_file intermediate_results_mkqa.json \
-    --dataset ./dataset/MKQA_eval.json
+# python -m src.eval.main \
+#     --config configs/eval_config_example.json \
+#     --baseline_type prompting \
+#     --output_file prompting_mkqa.json \
+#     --translation_file ./mt1_translations_mkqa.json \
+#     --intermediate_file intermediate_results_mkqa.json \
+#     --dataset ./dataset/MKQA_eval.json
 
 
-python -m src.eval.main \
-    --config configs/eval_config_example.json \
-    --baseline_type prompting \
-    --output_file prompting_mkqa_lora.json \
-    --translation_file ./mt1_translations_mkqa.json \
-    --intermediate_file intermediate_results_mkqa.json \
-    --dataset ./dataset/MKQA_eval.json \
-    --lora_path /export/fs05/dzhang98/model/MT2_lora/checkpoint-4500
+# python -m src.eval.main \
+#     --config configs/eval_config_example.json \
+#     --baseline_type prompting \
+#     --output_file prompting_mkqa_lora.json \
+#     --translation_file ./mt1_translations_mkqa.json \
+#     --intermediate_file intermediate_results_mkqa.json \
+#     --dataset ./dataset/MKQA_eval.json \
+#     --lora_path /export/fs05/dzhang98/model/MT2_lora/checkpoint-4500
 
 
 
