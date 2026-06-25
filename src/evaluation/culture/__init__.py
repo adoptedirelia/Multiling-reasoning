592e58380a77b7262b88ec7e21cea5a68723ddfc
Arnav Mazumder <am2107@klone-login01.hyak.local>
2026-06-24 23:46:55 -0700

    Clean repo

--- src/evaluation/culture/__init__.py
+++
@@ -1,8 +0,0 @@
-from .config import CultureEvalConfig, load_config
-from .runner import run
-
-__all__ = [
-    "CultureEvalConfig",
-    "load_config",
-    "run",
-]
--- src/train/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import TrainConfig
-from .trainer import MT2Trainer
-from .dataset import MT2Dataset
-
-__all__ = ['TrainConfig', 'MT2Trainer', 'MT2Dataset']
